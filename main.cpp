79057b212caa94b4d93d66e4f1e0d17b124945e1
aiden cox <cox.378@wright.edu>
2025-03-06 13:57:26 -0500

    basic pointers

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+//pointer is a variable that stores a memory address of another variable
+        //sometimes its easier to work with a n address
+
+    //& address of operator
+    // * dereference operator
+
+    std::string name = "Aiden";
+    int age = 20;
+    std::string freePizzas[5] = {"pizza1", "pizza2", "pizza3", "pizza4", "pizza5"};
+
+
+    std::string *ptrName = &name;
+    int *pAge = &age;
+    std::string *pFreePizzas = freePizzas;
+
+    std::cout << *ptrName;
+    std::cout << *pAge;
+    std::cout << *pFreePizzas;
+
+
+
+
     return 0;
 }
